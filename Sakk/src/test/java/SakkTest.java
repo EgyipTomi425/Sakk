17f612a66b1e8a721c2c64d2bf1bb82e61ae5904
egyiptomi425 <tomi04252002@gmail.com>
2023-04-04 22:09:16 +0200

    JUnit, slf4j, logback hozzáadása a függőségekhez. Verzió: 0.0.0

---
+++ Sakk/src/test/java/SakkTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tesztelésre használt osztály.
+ * 
+ * @author Kecyke
+ */
+public class SakkTest 
+{
+    @Test
+    public void osszeadastest() 
+    {
+        assertEquals(5, 3+2,"Összeadás sikertelen");
+    }
+    @Test
+    public void osszeadastest2() 
+    {
+        assertEquals(5, 3+2,"Összeadás sikertelen");
+    }
+}
